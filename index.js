f40e4b774ca1ae8c05cf099850716c236b877d83
Rithvik Vibhu <rithvikvibhu@gmail.com>
2018-05-07 12:58:54 +0530

    Separated functions, added support for multis

--- index.js
+++ index.js
@@ -1,34 +1,63 @@
+// process.env.NODE_ENV = 'testing';
+process.env.NODE_ENV = 'production';
+
 const Snooper = require('reddit-snooper')
 const request = require('request');
 const config = require('config')
 
-var counter = 0;
 const botName = 'darnbot';
 const accounts = JSON.parse(JSON.stringify(config.get('accounts')));
-const snooper = new Snooper(accounts[botName]);
+const multiName = config.get('multiName');
+var counter = 0;
+var subsList = [];
+var subs = '';
 
 console.log('Available accounts:', Object.keys(accounts));
-console.log('botName: '+botName);
+console.log('botName: '+ botName);
+console.log('multiName: '+ multiName);
 
-// Get latest count from bot's comment history
-request('https://www.reddit.com/user/'+botName+'/comments.rss', (err, res, body) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  
-  var re = /Darn[ \^]*Counter:[ \^]*(\d+)/gi;
-  var result = re.exec(body);
-  if (!result || !result[1]) {
-    console.log('Cannot get latest count. Exiting.');
-    console.log(body);
-    return;
-  }
-  console.log('Setting counter to', result[1]);
-  counter = parseInt(result[1])
-  
-  // Watch for new comments
-  snooper.watcher.getCommentWatcher('IsTodayFridayThe13th').on('comment', function(comment) {
+const snooper = new Snooper(accounts[botName]);
+
+function getSubs () {
+  return new Promise(function(resolve, reject) {
+    snooper.api.get(`/api/multi/user/${botName}/m/${multiName}`, {}, function (err, statusCode, res) {
+      if (err) {
+        console.log(err);
+        reject(err);
+      }
+      res.data.subreddits.forEach(function(sub) {
+        subsList.push(sub.name)
+      });
+      subs = subsList.join('+');
+      console.log('Listening on:', subsList);
+      resolve();
+    });
+  });
+}
+
+function getLatestCount () {
+  return new Promise(function(resolve, reject) {
+    request(`https://www.reddit.com/user/${botName}/comments.rss`, (err, res, body) => {
+      if (err) {
+        console.log(err);
+        reject(err);
+      }
+      var re = /Darn[ \^]*Counter:[ \^]*(\d+)/gi;
+      var result = re.exec(body);
+      if (!result || !result[1]) {
+        console.log('Cannot get latest count. Exiting.');
+        console.log(body);
+        reject();
+      }
+      console.log('Setting counter to', result[1]);
+      counter = parseInt(result[1]);
+      resolve();
+    });
+  });
+}
+
+function listenForComments () {
+  snooper.watcher.getCommentWatcher(subs).on('comment', function(comment) {
     console.log('u/' + comment.data.author + ' posted', comment.data.body);
     if (comment.data.author == botName) {return;}
     var count = (comment.data.body.match(/darn/gi) || []).length;
@@ -36,22 +65,23 @@ request('https://www.reddit.com/user/'+botName+'/comments.rss', (err, res, body)
     if (count) {
       counter += count;
       console.log('Updating counter to', counter);
-      // Reply to comment
-      snooper.api.post("/api/comment", {
-          api_type: "json",
-          text: `What a ***darn*** shame...
-
----
-^^Darn ^^Counter: ^^${counter} ^^| ^^DM ^^me ^^with: ^^'blacklist-me' ^^to ^^be ^^ignored`,
-          thing_id: comment.data.name
-      }, function (err, statusCode, data) {
-          if (!err) {
-            console.log('just replied to comment: ' + comment.data.name)
-          } else {
-            console.log(err);
-          }
-      })
+      postComment(comment);
     }
   }).on('error', console.error);
-  
-})
+}
+
+function postComment (ref) {
+  snooper.api.post("/api/comment", {
+      api_type: "json",
+      text: `What a ***darn*** shame...\n\n---\n^^Darn ^^Counter: ^^${counter} ^^| ^^DM ^^me ^^with: ^^'blacklist-me' ^^to ^^be ^^ignored`,
+      thing_id: ref.data.name
+  }, function (err, statusCode, data) {
+      if (!err) {
+        console.log('just replied to comment: ' + ref.data.name)
+      } else {
+        console.log(err);
+      }
+  })
+}
+
+getSubs().then(getLatestCount).then(listenForComments).catch(e => console.error(err));
