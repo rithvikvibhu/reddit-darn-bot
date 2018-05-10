a2d16c9e7f51a1b59560f6843f7f33bb02ea8e5e
Rithvik Vibhu <rithvikvibhu@gmail.com>
2018-05-10 16:49:35 +0530

    Some stats functions

--- database.js
+++ database.js
@@ -1,5 +1,6 @@
 const config = require('config');
 const mongoose = require('mongoose');
+const moment = require('moment');
 const Schema = mongoose.Schema;
 
 const dbCreds = config.get('database');
@@ -27,14 +28,74 @@ var commentSchema = new Schema({
   is_submitter: Boolean,
 });
 
-exports.Comment = mongoose.model('Comment', commentSchema);
-
+var Comment = mongoose.model('Comment', commentSchema);
 mongoose.connect(`mongodb://${dbCreds.username}:${dbCreds.password}@${dbCreds.host}:${dbCreds.port}/${dbCreds.db}`);
-
 var db = mongoose.connection;
+
+exports.Comment = Comment;
 exports.db = db;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('Connected to database.');
+  Promise.all([getCommentCountBySubreddit(), getFrequentAuthors()]).then(function(results) {
+    console.log(results);
+  }).catch(function(err) {console.error(err)});
 });
+
+function getCommentCountBySubreddit () {
+  return new Promise(function(resolve, reject) {
+    Comment.aggregate([
+      {
+        "$project": {
+          subreddit: 1,
+          _id: 0
+        }
+      },
+      {
+        "$group": {
+          _id: "$subreddit",
+          count: { $sum: 1 }
+        }
+      },
+      {
+        "$sort": {
+          count: -1
+        },
+      }
+    ], function(err, result) {
+      if (err) return reject(err);
+      resolve(result);
+    });
+  });
+}
+
+function getCommentCountByDate () {
+  var current = moment().unix();
+  var startOfDay = moment().unix();
+}
+
+function getFrequentAuthors () {
+  return new Promise(function(resolve, reject) {
+    Comment.aggregate([
+      {
+        "$project": {
+          author: 1,
+          _id: 0
+        }
+      },
+      {
+        "$group": {
+          _id: "$author",
+          count: { $sum:1 }
+        }
+      },
+      {
+        "$sort": { count: -1 }
+      }
+    ], function(err, result) {
+      if (err) return reject(err);
+      resolve(result);
+    });
+  });
+}
